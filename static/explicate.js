4567d230aa07a0c29d5f7d7b9b9998cd836dd3bb
Christian Brink <christianpbrink@gmail.com>
2014-12-15 16:01:36 -0500

    Further built out our provisional presentation layer.  We now have a notion of a subtree being 'in focus', the root subtree is in focus by default, and up/down keypresses change the focus.

--- static/explicate.js
+++ static/explicate.js
@@ -1,6 +1,11 @@
+var state = {
+  'currentPos': 0,
+  'theTree': {},
+};
+
 renderTree = function(tree) {
     var recVal = _.map(tree['branches'], renderTree);
-    var rv = '<div class="tree">'
+    var rv = '<div class="tree" data-id="' + tree['id'] + '" data-parent-id="' + tree['parentID'] + '">'
            +      '<div class="label">' + tree['label'] + '</div>'
            +      '<div class="branches">' + recVal.join('') + '</div>'
            +  '</div>'
@@ -29,6 +34,46 @@ demoTree = {
   ],
 };
 
+nextID = 0;
+function gimmeAnID() {
+  var theID = nextID;
+  nextID++;
+  return theID;
+}
+
+function giveIDs(parentID, tree) {
+  var id = gimmeAnID();
+  tree['id'] = id;
+  tree['parentID'] = parentID;
+  _.map(tree['branches'], function(branch) {
+      return giveIDs(id, branch);
+  });
+
+}
+
+giveIDs(false, demoTree);
+
+
+/*
+ *  Navigation / position utils 
+ */
+
+function getParentID(id, tree) {
+  return fmap(function(x) { return x['parentID']; }, treeForID(id, tree));
+}
+
+function getFirstChildID(id, tree) {
+    var maybeSubtree = treeForID(id, tree);
+    var maybeChildIDs = fmap(idsOfDirectChildren, maybeSubtree);
+    var maybeFirstID = join(fmap(maybeHead, maybeChildIDs));
+    return maybeFirstID;
+}
+
+function getLeftSibling(id, tree) {
+
+}
+
+
 function fmap(f, maybeVal) {
   if (isJust(maybeVal)) {
     return ['Just', f(fromJust(maybeVal))];
@@ -52,6 +97,61 @@ function join(maybeVal) {
         return false;
     }
 }
+
+function changePos(newPos) {
+  console.log("changePos >> newPos: ", newPos);
+  getDOMNodeByID(state['currentPos']).removeClass('focussed');
+  state['currentPos'] = newPos;
+  var newDOMNode = getDOMNodeByID(newPos);
+  console.log("changePos >> newDOMNode: ", newDOMNode);
+  newDOMNode.addClass('focussed');
+}
+
+function getDOMNodeByID(theID) {
+  return $('.tree').filter(function(i, elem) { return $(elem).attr('data-id') == theID; });
+}
+
+function keyHandler(event) {
+  switch(event.keyCode) {
+    case keyCode.LEFT:
+      break;
+    case keyCode.UP:
+      var maybeParentID = getParentID(state['currentPos'], demoTree);
+      console.log("keyHandler >> maybeParentID:", maybeParentID);
+      fmap(changePos, maybeParentID);
+      break;
+    case keyCode.RIGHT:
+      break;
+    case keyCode.DOWN:
+      var maybeFirstChildID = getFirstChildID(state['currentPos'], demoTree);
+      console.log("keyHandler >> maybeFirstChildID:", maybeFirstChildID);
+      fmap(changePos, maybeFirstChildID);
+      break;
+    default:
+      console.log('keyCode ' + event.keyCode);
+      break;
+    };
+}
+
+function idsOfDirectChildren(tree) {
+    return _.map(tree['branches'], function(branch) { 
+        return branch['id']; 
+    });
+}
+
+function treeForID(id, tree) {
+    if (tree.id == id) {
+        return ['Just', tree];
+    } else if (tree['branches']) {
+        var result = applyTillResult(tree['branches'], function(branch) {
+            return treeForID(id, branch);
+        });
+        return result;
+    } else {
+        return false;
+    }
+}
+
 function isJust(m) {
     return m.length && m[0] === 'Just';
 }
@@ -59,6 +159,16 @@ function isJust(m) {
 function fromJust(m) {
     return m[1];
 }
+
+function childIDs(id, tree) {
+    var maybeTree = treeForID(id, tree);
+    if (isJust(maybeTree)) {
+        return idsOfDirectChildren(fromJust(maybeTree));
+    } else {
+        console.error("childIDs >> Expected `Just _`, received `Nothing`");
+    }
+}
+
 keyCode = {
     BACKSPACE: 8,
     COMMA: 188,
@@ -82,4 +192,13 @@ keyCode = {
     SPACE: 32,
     TAB: 9,
     UP: 38
+}
+
+function applyTillResult(array, f) {
+  for (var i = 0 ; i < array.length ; i++) {
+    if(f(array[i])) {
+      return f(array[i]);
+    }
+  }
+  return false;
 }
